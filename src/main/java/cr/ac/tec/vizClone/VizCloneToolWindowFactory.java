20a0f26ca309fe4ed2e55343a754e6c22d3ad903
Jose Navas <jnavas@tec.ac.cr>
2023-09-25 20:03:49 -0600

    WIP: 1. showing code layout 2. showing clone layout with focus

--- src/main/java/cr/ac/tec/vizClone/VizCloneToolWindowFactory.java
+++ src/main/java/cr/ac/tec/vizClone/VizCloneToolWindowFactory.java
@@ -9,15 +9,16 @@ import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.xml.stream.Location;
 import java.awt.*;
 import java.util.Random;
 
 public class VizCloneToolWindowFactory implements ToolWindowFactory, DumbAware {
 
-    private static final String GREEN = "#8CD47E";
-    private static final String YELLOW = "#F8D66D";
-    private static final String ORANGE = "#FFB54C";
-    private static final String RED = "#FF6961";
+    private static final String GREEN = "#638409"; //"#8CD47E";
+    private static final String YELLOW = "#b59d32"; //"#F8D66D";
+    private static final String ORANGE = "#d67328"; //""#FFB54C";
+    private static final String RED = "#890c16"; //"#FF6961";
 
     private static final String[] SIMILITUDE = { GREEN, YELLOW, ORANGE, RED };
 
@@ -40,6 +41,7 @@ public class VizCloneToolWindowFactory implements ToolWindowFactory, DumbAware {
             contentPanel.add(codePanel, BorderLayout.NORTH);
             contentPanel.add(edgePanel, BorderLayout.CENTER);
             contentPanel.add(clonePanel, BorderLayout.SOUTH);
+            edgePanel.setClonePanel(clonePanel);
             //contentPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         }
 
@@ -62,29 +64,31 @@ public class VizCloneToolWindowFactory implements ToolWindowFactory, DumbAware {
             return r.nextInt(high - low) + low;
         }
 
+        Color translucent(Color color, int alpha) {
+            return new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+        }
+
         @Override
         protected void paintComponent(Graphics g) {
             if (getX() >= 0 && getY() >= 0) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                //Color color = Color.BLUE;// this.getParent().getBackground();
-                //GradientPaint gradientPaint = new GradientPaint(0, 0, Color.decode("#1CB5E0"), 0, getHeight(), Color.decode("#000046"));
-                //g2.setPaint(gradientPaint);
-                //g2.setColor(color.darker());
                 g2.setClip(null);
+                g2.setColor(g2.getBackground().darker());
+                g2.fillRect(0, 0, getWidth(), getHeight());
                 if (heights == null) {
                     heights = new int[getWidth()];
                     colors = new Color[getWidth()];
                     for (int i = 0; i < getWidth(); i++) {
                         int colorIdx = getNextRandom(0, 4);
                         colors[i] = Color.decode(VizCloneToolWindowFactory.SIMILITUDE[colorIdx]);
-                        heights[i] = getNextRandom(1, 5) + colorIdx * 5;                    }
+                        heights[i] = getNextRandom(1, 5) + colorIdx * 5;
+                    }
                 }
                 for (int i = 0; i < getWidth(); i++) {
                     g2.setColor(colors[i]);
                     g2.drawLine(i, 20-heights[i], i, 20);
                 }
-                //g2.fillRect(0, 0, getWidth(), getHeight());
                 super.paintComponent(g);
             }
         }
@@ -92,9 +96,27 @@ public class VizCloneToolWindowFactory implements ToolWindowFactory, DumbAware {
 
     private static class EdgePanel extends JPanel {
 
+        private ClonePanel clonePanel = null;
+
         public EdgePanel() {
             super();
             setOpaque(false);
+        }
+
+        public void setClonePanel(ClonePanel clonePanel) {
+            this.clonePanel = clonePanel;
+        }
+
+        private Point getZoomFrom() {
+            Point loc = new Point();
+            loc.setLocation(getWidth() % 200 / 2, getHeight() / 5 * 3);
+            return loc;
+        }
+
+        private Dimension getZoomTo() {
+            Dimension dim = new Dimension();
+            dim.setSize(getWidth(), getHeight() / 5);
+            return dim;
         }
 
         @Override
@@ -102,7 +124,7 @@ public class VizCloneToolWindowFactory implements ToolWindowFactory, DumbAware {
             if (getX() >= 0 && getY() >= 0) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                Color color = Color.RED;// this.getParent().getBackground();
+                Color color = this.getParent().getBackground();
                 //GradientPaint gradientPaint = new GradientPaint(0, 0, Color.decode("#1CB5E0"), 0, getHeight(), Color.decode("#000046"));
                 //g2.setPaint(gradientPaint);
                 g2.setColor(color.darker());
@@ -121,17 +143,41 @@ public class VizCloneToolWindowFactory implements ToolWindowFactory, DumbAware {
             this.setPreferredSize(new Dimension(0, minHeight));
         }
 
+        Random r = new Random();
+        int[] heights = null;
+        Color[] colors = null;
+
+        int getNextRandom(int low, int high) {
+            return r.nextInt(high - low) + low;
+        }
+
+        Color translucent(Color color, int alpha) {
+            return new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
+        }
+
         @Override
         protected void paintComponent(Graphics g) {
             if (getX() >= 0 && getY() >= 20) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                Color color = Color.GREEN;// this.getParent().getBackground();
-                //GradientPaint gradientPaint = new GradientPaint(0, 0, Color.decode("#1CB5E0"), 0, getHeight(), Color.decode("#000046"));
-                //g2.setPaint(gradientPaint);
-                g2.setColor(color.darker());
                 g2.setClip(null);
+                g2.setColor(g2.getBackground().darker());
                 g2.fillRect(0, 0, getWidth(), getHeight());
+                if (heights == null) {
+                    heights = new int[getWidth()];
+                    colors = new Color[getWidth()];
+                    for (int i = 0; i < getWidth(); i++) {
+                        int colorIdx = getNextRandom(0, 4);
+                        if (Math.abs(getWidth() / 2 - i) > 100)
+                            colors[i] = translucent(Color.decode(VizCloneToolWindowFactory.SIMILITUDE[colorIdx]), 64);
+                        else
+                            colors[i] = Color.decode(VizCloneToolWindowFactory.SIMILITUDE[colorIdx]);
+                        heights[i] = getNextRandom(1, 5) + colorIdx * 5;                    }
+                }
+                for (int i = 0; i < getWidth(); i++) {
+                    g2.setColor(colors[i]);
+                    g2.drawLine(i, 20-heights[i], i, 20);
+                }
                 super.paintComponent(g);
             }
         }
